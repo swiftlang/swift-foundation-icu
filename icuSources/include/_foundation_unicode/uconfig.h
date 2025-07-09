e133b7d76895f1b325b41d21e4b1fdbdee770623
Guillaume Lessard <glessard@tffenterprises.com>
2025-07-09 14:58:45 -0700

    Revert "_FoundationUnicode: apply symbol renaming for non-Darwin targets (#63)"
    This reverts commit 673a106f93936ce305af1c835904a79e71510309.

--- icuSources/include/_foundation_unicode/uconfig.h
+++ icuSources/include/_foundation_unicode/uconfig.h
@@ -101,11 +101,7 @@
 #ifndef U_DISABLE_RENAMING
 #if APPLE_ICU_CHANGES
 //rdar://60884991 #58 Replace installsrc patching with changes directly in header files
-#if defined(__APPLE__)
 #define U_DISABLE_RENAMING 1
-#else
-#define U_DISABLE_RENAMING 0
-#endif
 #else
 #define U_DISABLE_RENAMING 0
 #endif // APPLE_ICU_CHANGES
